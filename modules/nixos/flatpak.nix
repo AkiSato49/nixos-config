04e6954472d2913fd68f1e53781f680ce0c117b4
AkiSato49 <carlosakisato@gmail.com>
2026-04-28 17:21:57 +1000

    fix: wait for network before adding flathub repo

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -6,6 +6,8 @@
   # Run after boot to ensure flathub is added
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     path = [ pkgs.flatpak ];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
